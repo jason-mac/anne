67f3118a20baafd1e50e7ecbef363ee021fe2fc6
Jason Mac <jasonvanmac@gmail.com>
2024-05-11 21:44:50 -0700

    Finished headerfile Circle.h

--- Lab1/Circle.cpp
+++ Lab1/Circle.cpp
@@ -1,55 +1,74 @@
 #include "Circle.h"
 #include <iostream>
 using std::cout;
-
+using std::endl;
+// Default Constructor
+//  Description: Constructs an object of the Circle class
+//              with data members initialized to
+//              xCoordinate = 0;
+//              yCoordinate = 0;
+//              radius = 0.0; // >= 0.0
+//  Postcondition: 'xCoordinate = 0 && yCoordinate = 0 && radius = 0.0'
 Circle::Circle() {
   xCoordinate = 0;
   yCoordinate = 0;
-  radius = 10;
+  radius = 10.0;
 }
 
+// Precondition: radius >= 0.0
+// Postcondition: 'xCoordinate = x && yCoordinate = y && radius = r'
 Circle::Circle(int x, int y, double r) {
   xCoordinate = x;
   yCoordinate = y;
-  this->radius = r;
-}
-
-int Circle::getXCoordinate() const { return xCoordinate; }
-
-int Circle::getYCoordinate() const { return yCoordinate; }
-
-double Circle::getRadius() const { return radius; }
-
-void Circle::setYCoordinate(int y) { yCoordinate = y; }
-
-void Circle::setXCoordainte(int x) { xCoordinate = x; }
-
-void Circle::setRadius(double r) {
-  if (r < 0) {
-    radius = 10;
-    return;
-  }
   radius = r;
 }
 
-void Circle::move(int dx, int dy) {
-  xCoordinate = xCoordinate + dx;
-  yCoordinate = yCoordinate + dy;
-}
+//--- Getters ---
 
-bool Circle::intersect(Circle c) const {
-  int radiusSum = this->radius + c.getRadius();
-  int deltaX = abs(this->getXCoordinate() - c.getXCoordinate());
-  int deltaY = abs(this->getYCoordinate() - c.getYCoordinate());
-  int deltaRsquared = deltaX * deltaX + deltaY * deltaY;
-  if (deltaRsquared <= radiusSum) {
-    return true;
+int Circle::getYCoordinate() const { return yCoordinate; }
+int Circle::getXCoordinate() const { return xCoordinate; }
+double Circle::getRadius() const { return radius; }
+
+//--- Setters ---
+//
+// Description: Change x coordinate of the center of the circle
+// Postcondition: 'xCoordinate  = x'
+void Circle::setXCoordainte(int x) { xCoordinate = x; }
+
+// Description: Change y coordinate of the center of the circle
+// Postcondition: 'yCoordinate  = y'
+void Circle::setYCoordinate(int y) { yCoordinate = y; }
+
+// Description: Change radius of the circle
+// Precondition: r >= 0.0
+// Postcondition: 'radius = r or radius = 10.0 if r is invalid'
+void Circle::setRadius(double r) {
+  if (r < 0) {
+    radius = 10;
+  } else {
+    radius = r;
   }
-  return false;
 }
 
+// Description: 'this' x and y coordinates are shifted by integer amount
+// Postcondition: 'xCoordinate = xCoordinate + horiz && yCoordinate =
+// yCoordinate + vert'
+void Circle::move(int horiz, int vert) {
+  xCoordinate = xCoordinate + horiz;
+  yCoordinate = yCoordinate + vert;
+}
+// Displays content of 'this'
+// Postcondition: Contents of 'this' are displayed as "x = 'xCoordinate', y =
+// 'yCoordinate', radius = 'radius'"
 void Circle::displayCircle() const {
-  cout << "x coordinate: " << xCoordinate << " "
-       << "y coordinate: " << yCoordinate << " "
-       << "radius: " << radius;
+  cout << "x = " << xCoordinate << " y = " << yCoordinate
+       << " radius = " << radius << endl;
+}
+
+// Description: Checks if 'this' and given circle are intersecting
+// Postcondition: returns true if 'this' and c are intersecting, returns false
+// otherwise
+bool Circle::intersect(Circle c) const {
+  double radiusSum = this->radius + c.radius;
+  return true;
 }
--- Lab1/Circle.h
+++ Lab1/Circle.h
@@ -1,20 +1,60 @@
+/*
+ * Circle.h
+ *
+ * Class Description: Class modelling circle in the cartesian plane and data
+ * members xCoordinate and yCoordinate will represent the center of the circle
+ * object and radius will represent the circle's radius offerring interstion
+ * checker
+ *
+ * Class invartiant: radius >= 0.0
+ *
+ *
+ * Author: Jason Mac
+ * Modified: May 2024
+ *
+ */
+
 #ifndef CIRCLE_H
 #define CIRCLE_H
+
+// Class Defintion
+
 class Circle {
 private:
-  int yourmom;
   int xCoordinate;
   int yCoordinate;
-  double radius;
+  double radius; // >= 0.0
 
 public:
+  //--- Constructors ---
+
+  // Default Constructor
+  //  Description: Constructs an object of the Circle class
+  //              with data members initialized to
+  //              xCoordinate = 0;
+  //              yCoordinate = 0;
+  //              radius = 0.0; // >= 0.0
+  //  Postcondition: 'xCoordinate = 0 && yCoordinate = 0 && radius = 0.0'
   Circle();
+
+  // Precondition: radius >= 0.0
+  // Postcondition: 'xCoordinate = x && yCoordinate = y && radius = r'
   Circle(int, int, double);
+
+  //--- Getters ---
+
   int getYCoordinate() const;
   int getXCoordinate() const;
-  void setXCoordainte(int);
-  void setYCoordinate(int);
   double getRadius() const;
+
+  //--- Setters ---
+  // Description: Change x coordinate of the center of the circle
+  // Postcondition: 'xCoordinate  = x'
+  void setXCoordainte(int);
+  // Description: Change y coordinate of the center of the circle
+  // Postcondition: 'yCoordinate  = y'
+  void setYCoordinate(int);
+
   void setRadius(double);
   void move(int, int);
   void displayCircle() const;
