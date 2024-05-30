5e706022460a9e8e36d5e48947cf276f620be1ed
Jason Mac <jasonvanmac@gmail.com>
2024-05-29 22:37:25 -0700

    test driver

--- Assignments/A1/MyADTTestDriver.cpp
+++ Assignments/A1/MyADTTestDriver.cpp
@@ -1,7 +1,15 @@
 // clang-format off
+/* MyADTTestDriver.cpp - Assignment 1
+ * 
+ * Class Description: Testing our MyADT Data Collection
+ *
+ * Author: Jason Mac 
+ * Modified on: May 2024
+ */
 #include "MyADT.h"
 #include <cstdlib>
 #include <iostream>
+
 
 using std::cout;
 using std::endl;
@@ -18,6 +26,13 @@ using std::string;
 
 // Fills a MyADT object
 
+void printADT(MyADT& adt) { 
+  string dash = "----------------------------------------------------";
+  cout << dash << endl;
+  adt.print();
+  cout << dash << endl;
+  cout << endl;
+}
 // Test Driver function for MyADT class
 int main() {
 
@@ -25,8 +40,8 @@ int main() {
   string dash = "----------------------------------------------------";
 
   // Creating test object using default constructor. 
+  cout << "Create a valid default MyADT data collection -> testing constructor MyADT()." << endl;
   MyADT test;
-  cout << "Creating test MyADT object...." << endl;
 
   // Test: getElementCount() with default constructor
   cout << "Testing getElementCount() function with default constructor instantiated test object" << endl;
@@ -45,9 +60,9 @@ int main() {
   Profile p1copy = p1;
 
   // Test: Insert function. Expecting succesful insert
-  cout << "Testing insert function..." << endl;
+  cout << "Testing insert function" << endl;
   cout << dash << endl;
-  cout << "Attempting to insert " << p1.getUserName() << " into test object..." << endl;
+  cout << "Attempting to insert " << p1.getUserName() << " into test object" << endl;
   if (test.insert(p1)) {
     cout << "\t" << p1.getUserName() << " has been inserted! (Expected Result)" << endl;
     cout << "\tChecking element count after insertion, expecting 1 : test.getElementCount() = " << test.getElementCount() << endl;
@@ -55,6 +70,8 @@ int main() {
     cout << "\t" << p1.getUserName() << " could not be inserted (Unexpected Result)" << endl;
   }
   cout << endl;
+  cout << "Printing current state of test object" << endl;
+  printADT(test);
 
   // Test: Duplicate insertion. Expecting failure of duplicate insertion
   cout << "Testing if duplicate profile can be inserted by inserting " << p1copy.getUserName() << " again" << endl;
@@ -66,7 +83,10 @@ int main() {
     cout << "\tAttmempt of insertion of duplicate profile failed (Expected Result)" << endl;
   }
   cout << endl;
-
+  cout << "Printing current state of test object" << endl;
+  cout << "Expecting to only have " << p1.getUserName() << " in test data collection" << endl;
+  printADT(test);
+  
   // Insert remainder profiles
   cout << "Inserting four more profiles with 'j' usernames..." << endl;
   test.insert(p2);
@@ -76,11 +96,9 @@ int main() {
   cout << endl;
 
   // Test: Print function and element count. Expecting Created Profiles to be displyaed in ascending order
+  cout << "Printing current state of test object" << endl;
   cout << "Printing out test ADT instance (Expecting 5 profiles with username starting with j)" << endl;
-  cout << dash << endl;
-  test.print();
-  cout << dash << endl;
-  cout << endl;
+  printADT(test);
   cout << "Printing element count. Expected Result: 5, test.getElementCount() = " << test.getElementCount() << endl;
   cout << endl;
   
@@ -93,6 +111,8 @@ int main() {
     cout << "\t" << "Failed to insert past MAX_ELEMENTS capacity for character j (Expected Result)" << endl;
   }
   cout << endl;
+  cout << "Printing current state of test object" << endl;
+  cout << "Expecting to have 5 profiles starting with 'j', but no profile with " << extraJ.getUserName() << endl;
 
   // Insert variety of Profiles
   cout << "Inserting a variety of profiles into test..." << endl;
@@ -101,26 +121,22 @@ int main() {
   test.insert(p6);
   test.insert(p7);
   cout << endl;
+  cout << "Printing current state of test object" << endl;
+  printADT(test);
 
   // Test: Copy constructor (deep copy). Expecting test and copy to not be alises of each other
   cout << "Testing copy constructor by making a new object with MyADT copy = test" << endl;
   cout << dash << endl;
   MyADT copy = test;
-  cout << "Calling MyADT copy = test..." << endl;
+  cout << "Calling MyADT copy = test" << endl;
   cout << "A deep copy of test has been instantiated" << endl;
   cout << endl;
 
   cout << "Printing out test object" << endl;
-  cout << dash << endl;
-  test.print();
-  cout << dash << endl;
-  cout << endl;
+  printADT(test);
 
   cout << "Printing out copy object. Expected Result: Exact same print as test" << endl;
-  cout << dash << endl;
-  copy.print();
-  cout << dash << endl;
-  cout << endl;
+  printADT(test);
 
   // Test: Deep copy implementation. Expecting different print statements for test and copy objects
   cout << "Inserting a new profile into copy..." << endl;
@@ -137,6 +153,8 @@ int main() {
     cout << "\t" << p6.getUserName() << " has not been removed (Unexpected Result)" << endl;
   }
   cout << endl;
+  cout << "Printing current state of test object. Expected Result: p6.getUserName() is not in print statement" << endl;
+  printADT(test);
 
   // Print both test and copy after modifications
   cout << "Printing out both test and copy after inserting another profile into copy and removal of a profile from test" << endl; 
@@ -144,16 +162,10 @@ int main() {
   cout << endl;
 
   cout << "Printing out test after inserting a new profile into copy and removing random from test" << endl;
-  cout << dash << endl;
-  test.print();
-  cout << dash << endl;
-  cout << endl;
+  printADT(test);
 
   cout << "Printing out copy after inserting new profile into copy and removing random from test" << endl;
-  cout << dash << endl;
-  copy.print();
-  cout << dash << endl;
-  cout << endl;
+  printADT(copy);
 
 
   // Test: Remove function
@@ -169,7 +181,9 @@ int main() {
     cout << "\t" << p1.getUserName() << " has failed to be removed (Unexpected Result)" << endl;
   }
   cout << endl;
-
+  cout << "Printing current state of test object" << endl;
+  cout << "Expecting " << p1.getUserName() << " to not be a part of print statement" << endl;
+  printADT(test);
 
   // Removing non-existent profile. Expecting failure of removal of non-existent profile
   Profile doesNotExist("doesNotExist");
@@ -180,6 +194,8 @@ int main() {
     cout << "\t" << doesNotExist.getUserName() << " has failed to be removed (Expected Result)" << endl;
   }
   cout << endl;
+  cout << "Printing current state of test object" << endl;
+  cout << "Expecting same print as previous print" << endl;
 
   // Test: Removing and then re-adding the same profile. Expecting succesful removal and isnert of same profile
   cout << "Testing remove followed by re-insert of the same profile" << endl;
@@ -193,6 +209,7 @@ int main() {
       cout << "\t" << p2.getUserName() << " has failed to be removed (Unexpected Result)" << endl;
   }
   cout << endl;
+  cout << ""
 
   // Re-inserting p2
   cout << "Re-inserting " << p2.getUserName() << " into test object..." << endl;
@@ -229,16 +246,15 @@ int main() {
   // Test: removeAll function
   cout << "Testing out removeAll() on test" << endl;
   cout << "Before calling test.removeAll(), test.getElementCount() = " << test.getElementCount() << endl;
-  cout << "Calling test.removeAll()... " << endl;
+  cout << "Calling test.removeAll()" << endl;
   test.removeAll();
   cout << "Check getElementCount() after calling test.removeAll() (Expecting 0), test.getElementCount() = " << test.getElementCount() << endl;
   cout << endl;
 
 
-  // Check for empty array. Expecting empty print statement for test object
+  // Check for empty MyADT. Expecting empty print statement for test object
   cout << "Printing after calling test.removeAll() should print no elements" << endl;
-  cout << "Calling test.print()..." << endl;
-  test.print();
+  printADT(test);
   cout << endl;
   cout << "See nothing?? good!" << endl;
 
@@ -247,17 +263,10 @@ int main() {
   cout << dash << endl;
   
   cout << "Printing out test object" << endl;
-  cout << dash << endl;
-  test.print();
-  cout << dash << endl;
-  cout << endl;
+  printADT(test);
 
   cout << "Printing out copy object" << endl;
-  cout << dash << endl;
-  copy.print();
-  cout << dash << endl;
-  cout << endl;
-
+  printADT(copy);
   // End of test driver
   cout << "End of MyADT test driver" << endl;
   return 0;
