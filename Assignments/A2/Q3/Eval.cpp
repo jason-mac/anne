c8a59abb27f6e851d023753401e32791a573bd25
Jason Mac <jasonvanmac@gmail.com>
2024-06-14 19:51:39 -0700

    q4

--- Assignments/A2/Q3/Eval.cpp
+++ Assignments/A2/Q3/Eval.cpp
@@ -17,7 +17,7 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-// Evaluate operation and return corresponding token
+// Evaluate operation and return corresponding token with calculated value  
 Token evaluate(Stack<Token>& numstack, Stack<Token>& opstack) {
   Token newToken;
   newToken.tt = integer;
@@ -28,7 +28,7 @@ Token evaluate(Stack<Token>& numstack, Stack<Token>& opstack) {
   int value;
   Token op = opstack.pop();
 
-  // Perform operation depending on token op
+  // Perform operation depending on token op and its operator value
   switch(op.tt) { 
     // case plus
     case pltok:
@@ -47,7 +47,7 @@ Token evaluate(Stack<Token>& numstack, Stack<Token>& opstack) {
       value = num2 / num1;
       break;
     default:
-      value = -1;
+      value = 0;
   }
   newToken.val = value;
   return newToken;
@@ -63,33 +63,51 @@ int main () {
 
   t = S.getnext();
   while((t.tt != eof) || (!opstack.isEmpty())) {
+    // check if t is integer
     if(t.tt == integer) {
+      // push t to the number stack and get the next token t
       numstack.push(t);
       t = S.getnext();
+    // check if t is a left paranthesis
     } else if (t.tt == lptok) {
+      // push t to the operator stack and get the next token t
       opstack.push(t);
       t = S.getnext();
+    // check if t is a right paranthesis
     } else if (t.tt == rptok) {
       if(opstack.peek().tt == lptok) {
+        // if the top of operator stack is left paranthesis, pop it and get next token
         opstack.pop();
         t = S.getnext();
       } else {
+        // perform operation with top two values on numstack and top value on opstack
+        // receive the value from operation, store it in token and push it to numstack
         Token newToken = evaluate(numstack, opstack);
         numstack.push(newToken); 
       }
+      //if t is +, -, or eof
     } else if (t.tt == pltok || t.tt == mitok || t.tt == eof) {
+      // if opstack is nonempty and the top is either +, -, *, or /
       if((!opstack.isEmpty()) && ((opstack.peek().tt == pltok) || (opstack.peek().tt == mitok) || (opstack.peek().tt == asttok) || (opstack.peek().tt == slashtok))) {
+        // perform operation with top two values on numstack and top value on opstack
+        // receive the value from operation, store it in token and push it to numstack
         Token newToken = evaluate(numstack, opstack);
         numstack.push(newToken); 
       } else {
+        // push t to the opstack and get next token
         opstack.push(t);
         t = S.getnext();
       }
+      //if t is either * or /
     } else if (t.tt == asttok || t.tt == slashtok) {
+      // if the opstack is not empty and the its top value is either / or *
       if(!opstack.isEmpty() && ((opstack.peek().tt == slashtok) || (opstack.peek().tt == asttok))) {
+        // perform operation with top two values on numstack and top value on opstack
+        // receive the value from operation, store it in token and push it to numstack
         Token newToken = evaluate(numstack, opstack);
         numstack.push(newToken); 
       } else {
+        // push t to the opstack and get next token
         opstack.push(t);
         t = S.getnext();
       }
