7536aa79cf064779a5006343a7b54a162e26aee8
Jason Mac <jasonvanmac@gmail.com>
2024-06-22 09:38:46 -0700

    A3Q1

---
+++ Assignments/A3/Q2/pull.py
@@ -0,0 +1,21 @@
+"""
+Pulling script
+"""
+
+import subprocess
+
+
+def run():
+    """
+    run function
+    """
+    try:
+        subprocess.run(["make", "clean"], check=True)
+        subprocess.run(["git", "pull"], check=True)
+        subprocess.run(["make", "all"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    run()
