be8613f10595d1de6c7c2582b159f4856e43ebc0
Jason Mac <jasonvanmac@gmail.com>
2024-06-22 09:17:25 -0700

    lab7

---
+++ Lab7/script2.py
@@ -0,0 +1,22 @@
+"""
+Testing script
+"""
+
+import os
+import subprocess
+
+
+def run():
+    """
+    run function
+    """
+    try:
+        subprocess.run(["make", "clean"], check=True)
+        subprocess.run(["git", "pull"], check=True)
+        subprocess.run(["make", "all"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    run()
