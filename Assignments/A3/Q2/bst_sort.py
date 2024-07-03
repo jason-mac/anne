6b59e44f7459d5bb5fcc6709820b7a69cf3b1fcd
Jason Mac <jasonvanmac@gmail.com>
2024-07-02 17:39:53 -0700

    A3Q2

--- Assignments/A3/Q2/bst_sort.py
+++ Assignments/A3/Q2/bst_sort.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 """
-Sorting Script for BST's
+Sort file for bst insertion
 """
 
-import math
-import os
-import subprocess
+import sys
 
 
-def bst_sort(array, sort_array, index, middle, radius):
+def bst_sort(arr):
     """
     bst_sort function, sorts an array for a mostly balaned bst tree sort
     """
     # base case
-    if index == len(array):
-        return
-    if index % 2 == 1:
-        radius *= -1
-    sort_array[index] = array[middle + radius]
-    bst_sort(array, sort_array, index + 1, middle, radius)
+    new_arr = [None] * len(arr)
+    middle = len(arr) // 2
+    radius = 0
+    for i in range(len(arr)):
+        increment = 0
+        if i % 2 == 1:
+            increment = -radius
+        if i % 2 == 0:
+            increment = radius
+            radius += 1
+        new_arr[i] = arr[middle + increment]
+    return new_arr
 
 
 def merge(array, left, middle, right):
     """
-    merge function
+    merge routine
     """
     left_size = middle - left + 1
     right_size = right - middle
@@ -57,10 +61,10 @@ def merge(array, left, middle, right):
 
 def merge_sort(array, left, right):
     """
-    merge function
+    merge sort
     """
     if left < right:
-        middle = math.floor((left + right) / 2)
+        middle = (right + left) // 2
         merge_sort(array, left, middle)
         merge_sort(array, middle + 1, right)
         merge(array, left, middle, right)
@@ -70,15 +74,31 @@ def run():
     """
     run function
     """
-    file_name = "forbidden.txt"
-    with open(file_name, "r", encoding="utf-8") as file:
-        data = file.read()
+    read_file_name = input("Input a file name: ")
+    try:
+        with open(read_file_name, "r", encoding="utf-8") as file:
+            data = file.read()
+            print("File content successfully read.\n")
+    except FileNotFoundError:
+        print(f"Error: The file '{read_file_name}' does not exist.")
+        sys.exit(1)
     data_array = data.split()
-    print(data_array)
     merge_sort(data_array, 0, len(data_array) - 1)
-    bst_array = [None] * len(data_array)
-    bst_sort(data_array, bst_array, 0, math.floor(len(data_array) / 2), 0)
-    print(data_array)
+    print("You're file in sort order")
+    print(data_array, "\n")
+    print("Your file in order ready for bst insertion")
+    bst_array = bst_sort(data_array)
+    print(bst_array)
+    write_file_name = "myDataFile.txt"
+    print("Writing data into ", write_file_name, "...")
+    try:
+        with open(write_file_name, "w", encoding="utf-8") as file:
+            for item in bst_array:
+                if item is not None:
+                    file.write(item + "\n")
+        print(f"Data successfully written to '{write_file_name}'.")
+    except RuntimeError as e:
+        print(f"Error writing to file '{write_file_name}': {e}")
 
 
 if __name__ == "__main__":
