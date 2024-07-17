f5a381f580bb02c8571882f55a5ee8e8eaa168ed
Jason Mac <jasonvanmac@gmail.com>
2024-07-17 09:44:42 -0700

    practice makefile

--- TemplateTest/test.cpp
+++ TemplateTest/test.cpp
@@ -7,7 +7,10 @@ using std::endl;
 using std::nothrow;
 
 int main() {
-  Node<int> *n1 = new Node<int>(1);
+  int v1 = 1;
+  int v2 = 2;
+  int v3 = 3;
+  Node<int> *n1 = new Node<int>(v1);
   cout << n1->getData() << endl;
   return EXIT_SUCCESS;
 }
