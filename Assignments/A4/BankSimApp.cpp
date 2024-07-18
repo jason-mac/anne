c330081ea0cf898ed693faa0d215636941831b87
Jason Mac <jasonvanmac@gmail.com>
2024-07-18 14:52:04 -0700

    A4P1

--- Assignments/A4/BankSimApp.cpp
+++ Assignments/A4/BankSimApp.cpp
@@ -18,11 +18,39 @@ using std::cout;
 using std::ifstream;
 using std::nothrow;
 
+void processArrival(Event arrivalEvent, PriorityQueue<Event>* eventPriorityQueue, Queue<Event>* bankLine, 
+                    bool &tellerAvailable, unsigned int &currentTime) {
+  eventPriorityQueue->dequeue();
+  Event customer = arrivalEvent;
+  if(bankLine->isEmpty() && tellerAvailable) {
+    unsigned int departureTime = customer.getTime() + customer.getLength();
+    Event newDepartureEvent('D', departureTime);
+    eventPriorityQueue->enqueue(newDepartureEvent);
+    tellerAvailable = false;
+  } else {
+    bankLine->enqueue(customer);
+  }
+}
+
+void processDeparture(Event departureEvent, PriorityQueue<Event>* eventPriorityQueue, Queue<Event>* bankLine,
+                      bool &tellerAvailable, unsigned int &currentTime){
+  if(!(bankLine->isEmpty())) {
+    Event customer = bankLine->peek();
+    bankLine->dequeue();
+    unsigned int departureTime = currentTime + customer.getTime();
+    Event newDepartureEvent('D', departureTime);
+    eventPriorityQueue->enqueue(newDepartureEvent);
+  } else {
+    tellerAvailable = true;
+  }
+}
+
 
 int main(int argc, char* argv[]) {
-  unsigned int currentTime = 0;
-  PriorityQueue<Event>* priorityQueue = new (nothrow) PriorityQueue<Event>();
-  if(priorityQueue == nullptr) {
+  Queue<Event>* bankLine = new(nothrow) Queue<Event>();
+  PriorityQueue<Event>* eventPriorityQueue = new (nothrow) PriorityQueue<Event>();
+  bool tellerAvailable = true;
+  if(eventPriorityQueue == nullptr || bankLine == nullptr) {
     cout << "Failed Memory allocation. Terminationg program...";
     return EXIT_FAILURE;
   }
@@ -33,18 +61,31 @@ int main(int argc, char* argv[]) {
   string filename = "";
   string delimiter = " ";
   size_t pos = 0;
+  unsigned int timeOfDeparture = 0;
+  unsigned int timeOfService = 0;
   if(argc > 1) {
     filename = argv[1];
     ifstream myfile(filename);
     if(myfile.is_open()) {
-      pos = aLine.find(delimiter);
-      string timeString = aLine.substr(0, pos);
-      aLine.erase(0, pos + delimiter.length());
-      string lengthString = aLine;
-      int timeInt = stoi(timeString);
-      int lengthInt = stoi(lengthString);
-      Event anEvent('A', timeInt, lengthInt);
-      priorityQueue->enqueue(anEvent);
+      while(getline(myfile, aLine)) {
+        pos = aLine.find(delimiter);
+        string arrivalTimeString = aLine.substr(0, pos);
+        aLine.erase(0, pos + delimiter.length());
+        string transactionTimeString = aLine;
+        int arrivalTimeInt = stoi(arrivalTimeString);
+        int transactionTimeInt = stoi(transactionTimeString);
+        Event newArrivalEvent('A', arrivalTimeInt, transactionTimeInt);
+        eventPriorityQueue->enqueue(newArrivalEvent);
+      }
+      while(!(eventPriorityQueue->isEmpty())) {
+        Event newEvent = eventPriorityQueue->peek();
+        unsigned int currentTime = newEvent.getTime();
+        if(newEvent.getType() == 'A') {
+          processArrival(newEvent, eventPriorityQueue, bankLine, tellerAvailable, currentTime);
+        } else {
+          processDeparture(newEvent, eventPriorityQueue, bankLine, tellerAvailable, currentTime);
+        }
+      }
     } else {
       cout << "file could no be openned";
     }
