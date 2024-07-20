63bccfa330828bd7e795a12e1a23be64602dfa9c
Jason Mac <jasonvanmac@gmail.com>
2024-07-19 19:17:06 -0700

    A4P1

--- Assignments/A4/BankSimApp.cpp
+++ Assignments/A4/BankSimApp.cpp
@@ -25,7 +25,7 @@ void printEvent(const Event anEvent);
 void processArrival(Event arrivalEvent, PriorityQueue<Event>* eventPriorityQueue, Queue<Event>* bankLine, 
                     bool &tellerAvailable, unsigned int &currentTime);
 void processDeparture(Event departureEvent, PriorityQueue<Event>* eventPriorityQueue, Queue<Event>* bankLine,
-                      bool &tellerAvailable, unsigned int &currentTime);
+                      bool &tellerAvailable, unsigned int &currentTime, unsigned int &customersProcessed);
 
 int main() {
   simulate();
@@ -43,7 +43,7 @@ void simulate() {
   string aLine = "";
   string delimiter = " ";
   size_t pos = 0;
-  unsigned int customerCount = 0;
+  unsigned int customersProcessed = 0;
   unsigned int waitingTime = 0;
 
   while (getline(cin, aLine)) {
@@ -55,36 +55,46 @@ void simulate() {
       int transactionTimeInt = stoi(transactionTimeString);
       Event newArrivalEvent('A', arrivalTimeInt, transactionTimeInt);
       eventPriorityQueue->enqueue(newArrivalEvent);
-      customerCount++;
   }
 
   unsigned int currentTime = 0;
   cout << "Simulation Begins" << endl;
   while (!(eventPriorityQueue->isEmpty())) {
+    // Get newEvent from eventPriorityQueue
     Event newEvent = eventPriorityQueue->peek();
+
+    // Print the following incoming event
     printEvent(newEvent);
+
+    // Get currentTime
     currentTime = newEvent.getTime();
+
+    // Calculate the waiting time of the next incoming customer waiting in line, if there is one 
     if(newEvent.getType() == 'D') {
+      // If the bankLine is empty, no one is waiting
       try {
         waitingTime += currentTime - bankLine->peek().getTime(); 
       } catch (EmptyDataCollectionException& e) {}
     }
+    
+    // Process event depending if arrival or departure
     if (newEvent.getType() == 'A') {
         processArrival(newEvent, eventPriorityQueue, bankLine, tellerAvailable, currentTime);
     } else {
-        processDeparture(newEvent, eventPriorityQueue, bankLine, tellerAvailable, currentTime);
+        processDeparture(newEvent, eventPriorityQueue, bankLine, tellerAvailable, currentTime, customersProcessed);
     }
   }
-  double averageWaitingTime = static_cast<double>(waitingTime) / customerCount;
+  double averageWaitingTime = static_cast<double>(waitingTime) / customersProcessed;
   cout << "Simulation Ends" << endl;
   cout << endl;
   cout << "Final Statistics:" << endl << endl;;
-  cout << "    Total number of people processed: " << customerCount << endl;
+  cout << "    Total number of people processed: " << customersProcessed << endl;
   cout << "    Average amount of time spent waiting: " << averageWaitingTime << endl;
   delete eventPriorityQueue;
   delete bankLine;
 }
 
+// Printing event function
 void printEvent(const Event anEvent) {
   if(anEvent.getType() == 'A') {
     cout << "Processing an arrival event at time:  " << std::setw(4) << anEvent.getTime() << endl;;
@@ -95,28 +105,44 @@ void printEvent(const Event anEvent) {
 
 void processArrival(Event arrivalEvent, PriorityQueue<Event>* eventPriorityQueue, Queue<Event>* bankLine, 
                     bool &tellerAvailable, unsigned int &currentTime) {
+  // dequeue the event
   eventPriorityQueue->dequeue();
+
+  // Get customer data
   Event customer = arrivalEvent;
+
+  // If the bankLine is empty and a teller is available, process them and setup a departure event
   if(bankLine->isEmpty() && tellerAvailable) {
     unsigned int departureTime = currentTime + customer.getLength();
     Event newDepartureEvent('D', departureTime);
     eventPriorityQueue->enqueue(newDepartureEvent);
     tellerAvailable = false;
   } else {
+    // If the bankLine is not empty or there is not a teller avaible, add the customer to the bankline
     bankLine->enqueue(customer);
   }
 }
 
 void processDeparture(Event departureEvent, PriorityQueue<Event>* eventPriorityQueue, Queue<Event>* bankLine,
-                      bool &tellerAvailable, unsigned int &currentTime){
+                      bool &tellerAvailable, unsigned int &currentTime, unsigned int &customersProcessed){
+  // dequeue the top event
   eventPriorityQueue->dequeue();
+
+  //Increment customersProcessed after processing a departure event 
+  customersProcessed++;
   if(!(bankLine->isEmpty())) {
+    // The next customer in line we get from the front of the bankLine
     Event customer = bankLine->peek();
+    // Deqeue the front customer
     bankLine->dequeue();
+    // Calculate Departure time 
     unsigned int departureTime = currentTime + customer.getLength();
+    // Make new departure event with the given customer's departure data
     Event newDepartureEvent('D', departureTime);
+    // Add the event to the PriorityQueue
     eventPriorityQueue->enqueue(newDepartureEvent);
   } else {
+    // If the bank line is empty, then there is a teller available
     tellerAvailable = true;
   }
 }
