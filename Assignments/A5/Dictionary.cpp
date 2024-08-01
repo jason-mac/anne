73f8ca8e3ca5ffe22b7b11e5f250733432424c72
Jason Mac <jasonvanmac@gmail.com>
2024-08-01 13:21:31 -0700

    lol

--- Assignments/A5/Dictionary.cpp
+++ Assignments/A5/Dictionary.cpp
@@ -75,7 +75,7 @@ unsigned int Dictionary::hashFunction( string indexingKey ) {
 
   // Put your code here
   uint64_t indexingKeyInt = stoul(indexingKey);
-  const uint64_t PRIME = 31;
+  const uint64_t PRIME = 151;
   uint64_t hashCode = 0;
   while (indexingKeyInt > 0) {
       hashCode = hashCode * PRIME + (indexingKeyInt % 10);
