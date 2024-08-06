35792d394a092d78424b10d771d7d7fa7366d5d3
Jason Mac <jasonvanmac@gmail.com>
2024-08-06 10:11:14 -0700

    j

--- Assignments/new/testDriver_Q.cpp
+++ Assignments/new/testDriver_Q.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include "BinaryHeap.h"
 #include "Queue.h"
-#include "BinarHeap.h"
+#include <iostream>
 
 int main() {
-    return 0;
+  Queue<int> q;
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  q.enqueue(a);
+  q.enqueue(b);
+  q.enqueue(c);
+  BinaryHeap<int> heap;
+  heap.insert(a);
+  heap.insert(b);
+  heap.insert(c);
+  cout << q.peek() << " peek q" << endl;
+  cout << " heap retrive " << heap.retrieve();
+  return 0;
 }
